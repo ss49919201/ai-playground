1a0abaa127caf3198b41c49f2b1fc3d7625f22ec
Shinya Sakae <ss49919201@gmail.com>
2025-06-02 22:28:43 +0900

    add test

--- go-query-builder/builder_test.go
+++ go-query-builder/builder_test.go
@@ -22,12 +22,12 @@ func TestBuild(t *testing.T) {
 			t.Errorf(q, a)
 		}
 	}
-	// {
-	// 	builder := NewBuilder()
-	// 	q, a := builder.Where(And(EqID(1), EqName("name")), Or(EqID(2))).And(EqID(3)).Build()
-	// 	expect := q == "select * from posts where ((id = ? and name = ?) or id = ?) and id = ?" && reflect.DeepEqual(a, []any{1, "name", 2, 3})
-	// 	if !expect {
-	// 		t.Errorf("")
-	// 	}
-	// }
+	{
+		builder := NewBuilder()
+		q, a := builder.Where(EqID(1), And(And(EqID(2), EqID(3)), EqName("name"))).Build()
+		expect := q == "select * from posts where id = ? and ((id = ? and id = ?) and name = ?)" && reflect.DeepEqual(a, []any{1, 2, 3, "name"})
+		if !expect {
+			t.Errorf(q, a)
+		}
+	}
 }
